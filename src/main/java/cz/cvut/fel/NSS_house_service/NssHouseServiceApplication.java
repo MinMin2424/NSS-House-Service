c21c501fd953cfe9887e0fe197c8f6c1d542bb16
MinMin2424 <minatran2003@gmail.com>
2025-05-16 20:02:21 +0200

    Initial project

---
+++ src/main/java/cz/cvut/fel/NSS_house_service/NssHouseServiceApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.NSS_house_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NssHouseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NssHouseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/cvut/fel/NSS_house_service/NssHouseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.NSS_house_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NssHouseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
